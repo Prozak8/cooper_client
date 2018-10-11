486c585c4478641c3ca7135f388c154f7d173b53
Zak <zakary.howard94@gmail.com>
2018-10-11 14:30:26 +0200

    updated test to access login/signup through the Api & buttons to index

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,9 +4,18 @@ import { IonicModule, Platform } from 'ionic-angular';
 import { PlatformMock, StatusBarMock, SplashScreenMock } from 'ionic-mocks'
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { Http, BaseRequestOptions, RequestMethod } from '@angular/http';
+import { MockBackend } from '@angular/http/testing';
+import { Angular2TokenService } from 'angular2-token'
 
 describe('AppComponent', () => {
     let fixture, component;
+
+    let signInData = {
+        email: 'test@test.com',
+        password: 'password',
+        userType: String
+    };
 
     beforeEach(() => {
         TestBed.configureTestingModule({
@@ -17,11 +26,21 @@ describe('AppComponent', () => {
                 IonicModule.forRoot(MyApp)
             ],
             providers: [
+                BaseRequestOptions,
+                MockBackend,
+                {
+                    provide: Http,
+                    userFactory: (backend, defaultOptions) => {
+                        return new Http(backend, defaultOptions)
+                    },
+                    deps: [MockBackend, BaseRequestOptions]
+                },
                 { provide: Platform, useFactory: () => PlatformMock.instance() },
                 { provide: StatusBar, useFactory: () => StatusBarMock.instance() },
-                { provide: SplashScreen, useFactory: () => SplashScreenMock.instance() }
+                { provide: SplashScreen, useFactory: () => SplashScreenMock.instance() },
+                Angular2TokenService
             ]
-        })
+        });
 
         fixture = TestBed.createComponent(MyApp);
         component = fixture.componentInstance;
@@ -31,4 +50,29 @@ describe('AppComponent', () => {
         expect(component).toBeTruthy();
         expect(component instanceof MyApp).toEqual(true);
     });
-})
+
+    it('login method', inject([Angular2TokenService, MockBackend], (tokenService, mockBackend) => {
+
+        mockBackend.connections.subscribe(
+            c => {
+                expect(c.request.getBody()).toEqual(JSON.stringify(signInData));
+                expect(c.request.method).toEqual(RequestMethod.Post);
+                expect(c.request.url).toEqual('https://https://cooper-zdh-api.herokuapp.com/api/v1/auth/sign_in');
+            }
+        );
+
+        component.login(signInData);
+    }));
+
+    it('signOut method', inject([Angular2TokenService, MockBackend], (tokenService, mockBackend) => {
+
+        mockBackend.connections.subscribe(
+            c => {
+                expect(c.request.method).toEqual(RequestMethod.Delete);
+                expect(c.request.url).toEqual('https://https://cooper-zdh-api.herokuapp.com/v1/auth/sign_out');
+            }
+        );
+
+        component.logout();
+    }));
+});
